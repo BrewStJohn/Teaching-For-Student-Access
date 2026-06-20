b86784b2ab77b212c5dc8fb3f1e6a148fa284f12
dbrewstj <brewington.stjohn@gmail.com>
2026-06-19 17:13:07 -0700

    friday class

--- Friday/L5/Virus Spread Analysis/main.py
+++ Friday/L5/Virus Spread Analysis/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize Pygame
 pygame.init()
@@ -17,19 +18,66 @@ bac_num = 5
 split_time = 250
 clock = pygame.time.Clock()
 
+
+
+
+
+
+
 # CHALLENGE: Add the doctor and virus classes to their own file as modules.
 class Virus(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos, split_time):
         super().__init__()
+
+        # image, very common way to create a Surface
         self.image = pygame.image.load('Virus Spread Analysis\\virus.png')
         self.image = pygame.transform.smoothscale(self.image, (15, 15))
+
+        # Rect
         self.rect = self.image.get_rect()
         self.rect.center = pos
 
-    def update():
-        pass
+        # Speed
+        self.speed = pygame.math.Vector2(0,5)
+        self.speed.rotate_ip(random.randint(0, 360))
+
+        # Virus Duplication
+        self.time = 0 # how much time has passed 
+        self.split_time = split_time # how many seconds should pass before the
+        # virus will split
+
+    def update(self):
         # how do I get this guy to move around?
+        self.rect = self.rect.move(self.speed)
+        # the sprite doesnt move irradically. How?
+        # --> try rotating the vector periodically
+
+        # this code below is close, but needs some work
+        if self.rect.left < 0 or self.rect.right > width:
+            self.speed[0] = -self.speed[0]
+        if self.rect.top < 0 or self.rect.bottom > height:
+            self.speed[1] = -self.speed[1]
+
         # how can we get viruses to duplicate?
+        # NOTE: How often should the viruses duplicate?
+        # HINT: Create a new virus at the same location as this virus
+        # HINT: pygame.sprite.Sprite.groups()
+
+        # how do we keep track of how much time has passed?
+        # once the time has passed, duplicate the virus?
+        self.groups().add() #add a new virus
+
+
+
+
+
+
+
+
+
+
+
+
 
 def main():
     # Game loop
